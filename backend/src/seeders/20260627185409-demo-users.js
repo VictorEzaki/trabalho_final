dceb0eec296cce33fa3ce40b8bdf2f56166b173d
Victor <victorezaki19@gmail.com>
2026-06-27 17:38:00 -0300

    fix: corrigido seeder para utilizar o bcrypt também

--- backend/src/seeders/20260627185409-demo-users.js
+++ backend/src/seeders/20260627185409-demo-users.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const bcrypt = require('bcrypt');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -7,27 +9,27 @@ module.exports = {
       {
         name: "Admin",
         email: "admin@email.com",
-        password: "123456",
+        password: bcrypt.hashSync("123456", 10),
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: "João Silva",
         email: "joao@email.com",
-        password: "123456",
+        password: bcrypt.hashSync("123456", 10),
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         name: "Maria Souza",
         email: "maria@email.com",
-        password: "123456",
+        password: bcrypt.hashSync("123456", 10),
         createdAt: new Date(),
         updatedAt: new Date()
       }
     ]);
   },
-
+  
   async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete("users", {
       email: ["admin@email.com", "joao@email.com", "maria@email.com"]
